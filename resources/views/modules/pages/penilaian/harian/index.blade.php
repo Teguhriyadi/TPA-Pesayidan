3d07ec170c330d86ed72839c60b20db653989109
Teguhriyadi <ilham.teguh55@gmail.com>
2024-06-22 02:50:02 +0700

    Modified: Update

--- resources/views/modules/pages/penilaian/harian/index.blade.php
+++ resources/views/modules/pages/penilaian/harian/index.blade.php
@@ -1,6 +1,6 @@
 @extends('modules.layouts.main')
 
-@push('modules-title', 'Penilaian Harian')
+@push('modules-title', 'Penilaian ' . $kategori)
 
 @push('modules-css')
     <link href="{{ url('/theme') }}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
